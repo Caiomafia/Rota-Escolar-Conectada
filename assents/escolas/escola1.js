fedb0987e23798719a00d9fea6a8feea85698dca
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2023-07-06 00:11:57 -0300

    Commit Secundario

--- assents/escolas/escola1.js
+++ assents/escolas/escola1.js
@@ -16,3 +16,34 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+
+
+
+
+const colorButtons = document.querySelectorAll(".btnBuscar");
+const entregueColor = document.querySelectorAll(".btnEntregue");
+
+
+function changeColor(event) {
+  const button = event.target;
+  button.style.backgroundColor = "red" ;
+  button.innerHTML = "ok";
+  
+}
+
+colorButtons.forEach(function(button) {
+  button.addEventListener("click", changeColor );
+});
+
+
+function entreColor(event){
+    const button = event.target;
+    button.style.backgroundColor = " green";
+    button.innerHTML = "Enviado"
+}
+
+entregueColor.forEach(function(button) {
+    button.addEventListener("click" , entreColor)
+ })
