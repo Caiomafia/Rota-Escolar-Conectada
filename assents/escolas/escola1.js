dcdcae1806c6f5dc582a25911f6d82f8ede871e3
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2023-07-05 23:19:53 -0300

    Commit finalizado

--- assents/escolas/escola1.js
+++ assents/escolas/escola1.js
@@ -1,34 +1,4 @@
 
-
-const colorButtons = document.querySelectorAll(".btnBuscar");
-const entregueColor = document.querySelectorAll(".btnEntregue");
-
-
-function changeColor(event) {
-  const button = event.target;
-  button.style.backgroundColor = "red" ;
-  button.innerHTML = "ok";
-  
-}
-
-colorButtons.forEach(function(button) {
-  button.addEventListener("click", changeColor );
-});
-
-
-function entreColor(event){
-    const button = event.target;
-    button.style.backgroundColor = " green";
-    button.innerHTML = "Enviado"
-}
-
-entregueColor.forEach(function(button) {
-    button.addEventListener("click" , entreColor)
- })
-
-
-//btns
-
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
