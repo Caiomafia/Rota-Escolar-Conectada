414dc3e9e8f55dfa8716e4197663c0f6732f406d
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2023-07-07 13:14:09 -0300

    commit final

---
+++ assents/delta/delta.js
@@ -0,0 +1,49 @@
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+
+
+
+
+const colorButtons = document.querySelectorAll(".btnBuscar");
+const entregueColor = document.querySelectorAll(".btnEntregue");
+
+
+function changeColor(event) {
+  const button = event.target;
+  button.style.backgroundColor = "green" ;
+  button.innerHTML = "ok";
+  
+}
+
+colorButtons.forEach(function(button) {
+  button.addEventListener("click", changeColor );
+});
+
+
+function entreColor(event){
+    const button = event.target;
+    button.style.backgroundColor = " green";
+    button.innerHTML = "Enviado"
+}
+
+entregueColor.forEach(function(button) {
+    button.addEventListener("click" , entreColor)
+ })
